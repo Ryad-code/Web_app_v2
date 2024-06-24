9137efb7d79d0bf0b675cf681bf61fa93b909115
Mohamed-ryad LAOUEDJ <mlaouedj@made-f0Cr4s6.clusters.42paris.fr>
2024-06-24 14:34:54 +0200

    SETUP_ROUTER_FRONT

---
+++ front/app/src/components/MainContainer.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function MainContainer() {
+  return <div>bonjour</div>;
+}
+
+export default MainContainer;
--- front/app/src/main.tsx
+++ front/app/src/main.tsx
@@ -1,10 +1,43 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import App from "./App.tsx";
+import "./index.css";
+import MainContainer from "./components/MainContainer.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import * as React from "react";
+import ReactDOM from "react-dom/client";
+import {
+  createRoutesFromElements,
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+  Link,
+} from "react-router-dom";
+
+const Root = () => {
+  return (
+    <div>
+      <Link to={"/HomePage"} className="nav-link">
+        {" "}
+        Home <App />
+      </Link>
+      <MainContainer />
+      <Outlet />
+    </div>
+  );
+};
+
+// Configure nested routes with JSX
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route path="/" element={<Root />}>
+      {/* <Route path="/Homepage" element={<HomePage />} /> */}
+      {/* <Route path="/Seconpage" element={<SecondPage />} /> */}
+    </Route>
+  )
+);
+
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+
+root.render(<RouterProvider router={router} />);
